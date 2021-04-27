f38958210aea5ab845a92b8a2565ec29fedc9dfe
Negarbsh <mehrnegar81@gmail.com>
2021-04-27 17:44:43 +0430

    add deck, card, graveYard

---
+++ src/main/java/model/GraveYard.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+
+public class GraveYard {
+    private ArrayList<Card> cardsInGraveYard;
+
+    public ArrayList<Card> getCardsInGraveYard() {
+        return cardsInGraveYard;
+    }
+
+    public void addCard(Card card){
+
+    }
+
+    public void removeCard(Card card){
+
+    }
+
+    public void showMonsters(){
+
+    }
+
+    public void selectCard(String cardAddress){
+
+    }
+
+    public void deselectCard(String cardAddress){
+
+    }
+
+    @Override
+    public String toString() {
+        return "GraveYard{}";
+    }
+}
+
