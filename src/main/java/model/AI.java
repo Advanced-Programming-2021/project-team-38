bd1ab68819fdca73de1e3aedfd95b9629779bf9e
shima <shimaamir2002@gmail.com>
2021-06-10 23:29:31 +0430

    message

---
+++ src/main/java/model/AI.java
@@ -0,0 +1,18 @@
+//package model;
+//
+//public class AI extends Player{
+//    Board rivalBoard;
+//    Board myBoard;
+//
+////    public AI(Board firstPlayer , Board secondPlayer) {
+////
+////    }
+//
+//    public void mainPhase(){
+//
+//    }
+//    public void battlePhase() {
+//
+//    }
+//
+//}
